de0d01e15aa5f49d2daa8f7bf1a81425678b7b73
Enes Eren Demirpolat <enes_erendemirpolat@hotmail.com>
2025-04-28 13:52:15 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the CI/CD Example App</h1>
+      </header>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />); 
